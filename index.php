667bb7469dd73ac92597f390d36c92011eb6d75b
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-08-04 11:34:42 +0200

    Update file structure with index.php, Add beer.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+require 'beverage.php';
+require 'beer.php';
+
+$mojito = new Beverage('Mojito', ['green', 10 . ' €' , 'brainfreeze warning']);
+
+$mojito -> getInfo();
+?>
