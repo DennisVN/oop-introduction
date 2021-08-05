28ce15e0fbb4abafffff68773eae8877787a3e8d
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-08-05 11:24:51 +0200

    Complete and understand ex 3. Proceed to nex project to avoid getting behind

--- exercise_3_private.php
+++ exercise_3_private.php
@@ -5,19 +5,37 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-class Beer extends Beverages
+class StrongBeer extends Beverages
 {
-    public string $name;
-    public float $alcohol;
+    private string $name;
+    private float $alcohol;
 
     public function __construct(string $color, float $price, string $temperature,  string $name,  float $alcohol)
     {
-        parent::__construct($color, $price, $temperature);
+        parent::__construct($color, $price, $temperature = "cold");
         $this->name = $name;
         $this->alcohol = $alcohol;
     }
     public function getAlcoholPercent()
     {
-        echo "{$this->alcohol}<br>";
+        echo "Alcohol percentage : {$this->alcohol}%<br>";
+    }
+    public function getName()
+    {
+        echo "Name: {$this->name} <br>";
+    }
+    public function setColor($color)
+    {
+        $this->color = $color;
+        echo "Color : {$this->color}<br>";
+    }
+    private function beerInfo()
+    {
+        echo "Hi i'm {$this->name} and am {$this->alcohol} strong which is pretty nice for a {$this->color} beer.";
+    }
+    public function getBeerInfo()
+    {
+        return $this->beerInfo();
     }
 }
+?>
--- index.php
+++ index.php
@@ -9,15 +9,20 @@ error_reporting(E_ALL);
 require "exercise_1_classes.php";
 require "exercise_2_extending.php";
 require "exercise_3_private.php";
-
+echo "exercise 1 <br>";
 $cola = new Beverages("black", 2);
 $cola->getInfo();
 $cola->temperature;
-
+echo "exercise 2 <br>";
 $duvel = new Beer("blonde", 3.5, "ice cold", "Duvel", 8.5);
 $duvel->getAlcoholPercent();
 echo "{$duvel->getAlcoholpercent()}";
 $duvel->getInfo();
 $duvel->color;
-
+echo "exercise 3 <br>";
+$duvel = new StrongBeer("blonde", 3.5, "ice cold", "Duvel", 8.5);
+$duvel->getAlcoholPercent();
+$duvel->getName();
+$duvel->setColor("light");
+$duvel->getBeerInfo($duvel);
 ?>
